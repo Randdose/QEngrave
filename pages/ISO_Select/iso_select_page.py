b66062503b4a4a3c1a650d27ca7f33b1a6f48ff5
Randdose <randomnes037@gmail.com>
2023-06-28 19:34:33 +0400

    removed commented code

--- pages/ISO_Select/iso_select_page.py
+++ pages/ISO_Select/iso_select_page.py
@@ -4,7 +4,9 @@ from PySide2.QtWidgets import (
 	QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QPushButton, QCheckBox, QLineEdit
 )
 
-from pages.ISO_Select.select_os_online import SelectOsOnCont
+# Import pages
+from pages.ISO_Select.select_os import SelectOsCont
+#from pages.ISO_Select.os_info import OsInfo
 
 # ISO selection container
 class IsoSelectPage(QWidget):
@@ -13,25 +15,10 @@ class IsoSelectPage(QWidget):
 
 		# Layout for the whole page
 		rootLayout = QVBoxLayout()
-
-		selectOsOnCont = SelectOsOnCont()
-		rootLayout.addWidget(selectOsOnCont)
 		self.setLayout(rootLayout)
 
+		selectOsOnCont = SelectOsCont()
+		rootLayout.addWidget(selectOsOnCont)
 
-		# IsoLayout = QVBoxLayout()
-		# self.setLayout(IsoLayout)
-  #
-		# self.setTitle("Select ISO")
-  #
-		# # Line containing path to selected file
-		# ISOFileLine = QLineEdit()
-  #
-		# # Button to select ISO file
-		# selectIsoBtn = QPushButton("Select")
-		# selectIsoBtn.clicked.connect(lambda : selectFile(ISOFileLine))
-  #
-		# IsoLayout.addWidget(selectIsoBtn)
-		# IsoLayout.addWidget(ISOFileLine)
-
-
+		# osInfo = OsInfo()
+		# rootLayout.addWidget(osInfo)
