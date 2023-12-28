218890d8e5a15e2e9501525b870c01108a4fe21e
Randdose <randomnes037@gmail.com>
2023-12-29 02:07:08 +0400

    added IsoInfo box. import os, to get size of file and other info. added getInfo method to IsoInfo box

--- sections/ISO_Select.py
+++ sections/ISO_Select.py
@@ -1,10 +1,13 @@
 from PySide2.QtCore import Qt, QProcess, QFileSelector
 from PySide2.QtGui import QIcon
 from PySide2.QtWidgets import (
-	QWidget, QPushButton, QCheckBox, QLineEdit, QGroupBox, QVBoxLayout,
+	QWidget, QLabel, QPushButton, QCheckBox, QLineEdit, QGroupBox, QVBoxLayout,
 	QHBoxLayout, QComboBox, QFileDialog
-	)
-from tools.SysTools import getDrives
+)
+
+from tools.SysTools import getDrives, commandOutput
+
+import os
 
 class IsoSelect(QGroupBox):
 	def __init__(self):
@@ -18,19 +21,55 @@ class IsoSelect(QGroupBox):
 		IsoSelectContLayout = QHBoxLayout()
 		IsoSelectCont.setLayout(IsoSelectContLayout)
 
-		IsoSelectBtn = QPushButton(
-			QIcon().fromTheme("document-open"), "Select ISO file"
-		)
-		IsoSelectLine = QLineEdit()
+		self.IsoSelectLine = QLineEdit()
+		IsoSelectBtn = QPushButton(QIcon().fromTheme("document-open"), None)
 
-		IsoSelectBtn.clicked.connect(lambda: self.showFileDial())
+		IsoSelectBtn.clicked.connect(lambda: self.getIso())
 
+		IsoSelectContLayout.addWidget(self.IsoSelectLine)
 		IsoSelectContLayout.addWidget(IsoSelectBtn)
-		IsoSelectContLayout.addWidget(IsoSelectLine)
+
+		self.IsoInfo = QGroupBox("ISO Info")
+		self.IsoInfoLayout = QVBoxLayout()
+		self.IsoInfo.setLayout(self.IsoInfoLayout)
 
 		rootLayout.addWidget(IsoSelectCont)
+		rootLayout.addWidget(self.IsoInfo)
 
-	def showFileDial(self):
+		try:
+		 self.refreshInfo()
+		except:
+			print("1")
+
+	def getInfo(self):
+
+		def clearInfoBox():
+			if self.IsoInfoLayout is not None:
+				while self.IsoInfoLayout.count():
+					item = self.IsoInfoLayout.takeAt(0)
+					widget = item.widget()
+					if widget is not None:
+						widget.deleteLater()
+					else:
+						self.clearLayout(item.layout())
+		clearInfoBox()
+
+		stat = os.stat(self.IsoSelectLine.text())
+		info = {
+			"Size": stat.st_size
+		}
+		readableInfo = {
+			"Size": f"{round(int(stat.st_size) / 1073742000, 2)} GiB" if int(stat.st_size) > 1073742000 else f"{round(int(stat.st_size) / 1048576, 2)} MiB"
+		}
+
+		for key in readableInfo.keys():
+				infoLabel = QLabel(f"{key}: {readableInfo[f'{key}']}")
+				self.IsoInfoLayout.addWidget(infoLabel)
+
+	def getIso(self):
 		dial = QFileDialog
-		fileName = dial.getOpenFileName(self, "open ISO file", "$HOME", "ISO files (*.iso)")
-		print(fileName)
+		fileName = dial.getOpenFileName(
+			self, "open ISO file", commandOutput("echo $HOME"), "ISO files (*.iso)"
+		)
+		self.IsoSelectLine.setText(fileName[0])
+		self.getInfo()
